1f0df1b81a5d009213bf8172a0776ffc84d35556
Jubel13 <jubelsinaga13@gmail.com>
2022-03-23 12:07:47 +0700

    Create imgae kit test

--- controllers/imagekitAuthController.js
+++ controllers/imagekitAuthController.js
@@ -1,16 +1,16 @@
-const ImageKit = require('imagekit');
+const ImageKit = require("imagekit");
 
 const imagekit = new ImageKit({
-  urlEndpoint: 'https://ik.imagekit.io/iqpgx3omg7kg/auto-classic',
-  publicKey: 'public_BACW09RMefisOpJUrHcxl/9Id9g=',
-  privateKey: 'private_m8l+5XTYExKEbUee/bgFrxEd59E='
+  urlEndpoint: process.env.IMAGE_KIT_URL,
+  publicKey: process.env.IMAGE_KIT_PUBLIC,
+  privateKey: process.env.IMAGE_KIT_PRIVATE,
 });
 
 const imagekitAuth = (req, res) => {
   let result = imagekit.getAuthenticationParameters();
   res.send(result);
-}
+};
 
 module.exports = {
-  imagekitAuth
-}
+  imagekitAuth,
+};
